a95ce1e7f962f2073d1e717f549be16fde948f53
20Gamer07 <Abdullozodaamirjon07@gmail.com>
2024-12-29 17:39:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const exp = require ('express')
+
+
+const app = exp()
+
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    port: "3306",
+    user: "gen_user",
+    host: "185.200.241.27",
+    database: "test_ustod",
+    password: "4rX&cHtw:uy&,l"
+});
+
+app.get('/', function(req,res) {
+    connection.quary('SELECT * FROM products', (err, rows,fields)=>{
+        if(!err)
+            res.send(rows)
+        else 
+        console.log(err)
+        
+    })
+})
+
+
+app.listen(8000, () =>{
+    console.log("server started");
+})
